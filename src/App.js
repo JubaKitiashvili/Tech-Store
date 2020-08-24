49072c1381278f9608ca70092d0ea3d57c488fe1
jubakitiashvili <juba.kitiashvili@gmail.com>
2020-08-24 17:50:22 +0400

    nav bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+// CSS
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// Pages
+import Home from './pages/HomePage';
+import About from './pages/AboutPage';
+import Products from './pages/ProductsPage';
+import Cart from './pages/CartPage';
+import Contact from './pages/ContactPage';
+import SingleProduct from './pages/SingleProductPage';
+import Default from './pages/Default';
+
+// Page Components
+import NavBar from './components/NavBar';
+import SideBar from './components/SideBar';
+import Footer from './components/Footer';
+import SideCart from './components/SideCart';
 
 function App() {
-  return <h1>tech store</h1>;
+  return (
+    <>
+      {/* navbar, sidebar, cart, footer */}
+      <NavBar />
+      <SideBar />
+      <SideCart />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/products" exact component={Products} />
+        <Route path="/products/:id" component={SingleProduct} />
+        <Route path="/cart" component={Cart} />
+        <Route component={Default} />
+      </Switch>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/Default.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Defaults() {
+  return (
+    <>
+      <h1>hello from Defaults</h1>
+    </>
+  );
+}
