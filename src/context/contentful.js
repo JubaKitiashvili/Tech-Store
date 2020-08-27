05bdd608e3c87ee0a67ec1fd8a57d47bd5fba05a
jubakitiashvili <juba.kitiashvili@gmail.com>
2020-08-27 14:20:28 +0400

    env

--- src/context/contentful.js
+++ src/context/contentful.js
@@ -1,6 +1,6 @@
 import * as contentful from 'contentful';
 
 export const client = contentful.createClient({
-  accessToken: 'VBDjkG-TfYdfLjHSVUjAUR1MmbuzFV9_xGuSSIl16-E',
-  space: 'nqstiznmjxru',
+  space: process.env.REACT_APP_SPACE_ID,
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN,
 });
