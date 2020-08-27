03ab73e9d4d63c0c0670fd16bdd5c54e1b0a1b12
jubakitiashvili <juba.kitiashvili@gmail.com>
2020-08-27 14:12:47 +0400

    img api

---
+++ src/context/contentful.js
@@ -0,0 +1,6 @@
+import * as contentful from 'contentful';
+
+export const client = contentful.createClient({
+  accessToken: 'VBDjkG-TfYdfLjHSVUjAUR1MmbuzFV9_xGuSSIl16-E',
+  space: 'nqstiznmjxru',
+});
